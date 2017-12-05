bc2b0566d15d86a98fdc930e57c9e9be40e8b56f
Heena <heenat2@ilinois.edu>
2017-12-04 18:47:59 -0800

    Making UI dropdown query integration

--- src/app.py
+++ src/app.py
@@ -13,6 +13,7 @@ app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 global search
 global lda_inference
+global w2v
 
 
 class ReusableForm(Form):
@@ -23,7 +24,12 @@ class ReusableForm(Form):
 
 def get_topic_string(results):
     top_topics = lda_inference.infer_topics(results)
-    return '::'.join(map(lambda x: x + ":w2v1/w2v2", top_topics))
+    return '::'.join(map(lambda x: x + ":" + get_w2v_similarity(x), top_topics))
+
+
+def get_w2v_similarity(term):
+    #call w2v with term to get sims
+    return '/'.join(['sim1', 'sim2'])
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -32,15 +38,17 @@ def hello():
         query = request.form['query']
         page = request.form['pageNum']
         state = request.form['state']
-        new_form = ReusableForm(request.form)
-        new_form.pageNum = page
+        if not page:
+            page = 0
         print query
         print page
         print state
-        search_results = search.get_results_for_query(query, max_results=10)
+        search_results = search.get_results_for_query(query + " " + ' '.join(state.split("::")), max_results=100)
         topic_string = get_topic_string(search_results)
-        new_form.state = topic_string
         flash(topic_string)
+        new_form = ReusableForm(request.form)
+        new_form.pageNum = page
+        new_form.state = topic_string
         flash("1::2")
         for doc in search_results:
             flash(Markup("<a href='" + doc.get_url() + "'>"
@@ -58,4 +66,6 @@ if __name__ == "__main__":
     global lda_inference
     lda_inference = TopicInference('/home/rik/Heena/corpusdata.dictionary',
                                    '/home/rik/Heena/LDA Model 50 Sym/lda_model_50_sym')
+    global w2v
+    w2v = None
     app.run(threaded=False)
